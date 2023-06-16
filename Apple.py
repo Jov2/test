9050e0aa9d90df721b7a57bce0495142d85a0e49
dimsa <dimitar.jovevski@gmail.com>
2023-06-16 13:48:30 +0200

    app.py
    App,py document

--- Apple.py
+++ Apple.py
@@ -1,3 +1,4 @@
-msg = "Hello World"
-print(msg)
-
+access_token = "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"
+g = Github(access_token)
+current_user = g.get_user()
+print(current_user.name)
